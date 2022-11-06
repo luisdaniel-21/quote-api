b91a23d80436f4327a4a34c7cb225a52febc48da
luisdaniel-21 <pbohorquezluisdani@gmail.com>
2022-11-06 14:27:01 -0600

    Configuracion de las bases de datos en MariaDB y mongo

--- config/connectors.ts
+++ config/connectors.ts
@@ -4,14 +4,14 @@ import { MongoClient, ClientMariaDB }
 
 
 const mongoClient = new MongoClient();
-const URI_MONGO="mongodb://itvoDeveloper:********@localhost:27017/quotes?authMechanism=SCRAM-SHA-256&authSource=admin";
+const URI_MONGO="mongodb://daniel:12345@localhost:27017/quotes?authMechanism=SCRAM-SHA-256&authSource=quotes";
 export const connectorMongoDB = await mongoClient.connect(URI_MONGO);
 
 const CONN_MARIADB = {
     hostname: "localhost",
-    port: 3308,
+    port: 3306,
     username: "devDeno",
-    password: "**********",
+    password: "t0ps3cr3t",
     db: "quotes",
     poolSize: 10};
 
